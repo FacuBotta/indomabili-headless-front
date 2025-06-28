be1f1d1104de2c5a22ef5971ddf3c1de7680e324
facubotta <nutuestampados@gmail.com>
2025-06-28 12:15:55 +0200

    update graphiql url

--- src/scripts/data-fetcher.mjs
+++ src/scripts/data-fetcher.mjs
@@ -2,13 +2,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export async function fetchGraphQLData(query, variables = {}) {
-  const response = await fetch(process.env.GRAPHQL_ENDPOINT, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ query, variables }),
-  });
+  const response = await fetch(
+    process.env.GRAPHQL_ENDPOINT ||
+      'https://www.lab.facudev.fr/wordpress/graphql',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query, variables }),
+    }
+  );
 
   const result = await response.json();
   return result.data;
